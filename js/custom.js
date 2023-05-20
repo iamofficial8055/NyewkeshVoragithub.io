4874f2c7b41f4fcd6362fb0e46c4b6dbfca6dfa0
mrofficial <nyvora8055@outlook.com>
2023-05-20 16:41:18 +0530

    mrofficial

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	"use strict"; // start of use strict
+
+	/*==============================
+	Menu
+	==============================*/
+	$('.header__btn').on('click', function() {
+		$(this).toggleClass('header__btn--active');
+		$('.header').toggleClass('header--active');
+		$('.sidebar').toggleClass('sidebar--active');
+	});
+});
